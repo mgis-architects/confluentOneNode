02651b8424f5dc89b709f293be660ddc5ce22882
adeturner <adrian.turner@maersk.com>
2017-03-17 10:23:45 +0000

    ...

--- confluentOneNode-build.sh
+++ confluentOneNode-build.sh
@@ -571,15 +571,15 @@ function topics.list() { curl -X GET -H "Content-Type: application/json" ${confl
 function topic.get() { curl -X GET -H "Content-Type: application/json" ${confluentRestUrl}/topics/${1} 2>/dev/null | python -mjson.tool; }
 function partition.list() { curl -X GET -H "Content-Type: application/json" ${confluentRestUrl}/topics/${1}/partitions 2>/dev/null | python -mjson.tool; }
 function partition.get() { curl -X GET -H "Content-Type: application/json" ${confluentRestUrl}/topics/${1}/partitions/${2} 2>/dev/null | python -mjson.tool; }
-function consumer.createbinary() { group=$1; instance=$2; echo '{ "id": "'$instance'", "format": "binary", "auto.offset.reset": "smallest", "auto.commit.enable": "false" }' > /tmp/consumer.create.$$.json; curl -X POST -H "Content-Type: application/vnd.kafka.binary.v1+json" --data-binary "@/tmp/consumer.create.$$.json" ${confluentRestUrl}/consumers/${group} 2>/dev/null | python -mjson.tool; }
-function consumer.createavro() { group=$1; instance=$2; echo '{ "id": "'$instance'", "format": "binary", "auto.offset.reset": "smallest", "auto.commit.enable": "false" }' > /tmp/consumer.create.$$.json; curl -X POST -H "Content-Type: application/vnd.kafka.avro.v1+json" --data-binary "@/tmp/consumer.create.$$.json" ${confluentRestUrl}/consumers/${group} 2>/dev/null | python -mjson.tool; }
-function consumer.readbinary() { group=$1; instance=$2; topic_name=$3; curl -X GET -H "Content-Type: application/vnd.kafka.binary.v2+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
-#function consumer.readavro() { group=$1; instance=$2; topic_name=$3; curl -X GET -H "Content-Type: application/vnd.kafka.avro.v1+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
-function consumer.readavro() { group=$1; instance=$2; topic_name=$3; curl -X GET -H "Accept: application/vnd.kafka.avro.v1+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
-function consumer.readavro() { group=$1; instance=$2; topic_name=$3; curl -X GET -H "Accept: application/vnd.kafka.v2+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
-function consumer.readavro() { group=$1; instance=$2; topic_name=$3; curl -X GET -H "Accept: application/vnd.kafka.binary.v1+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
-function consumer.commit() { group=$1; instance=$2; curl -X POST -H "Content-Type: application/json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/offsets 2>/dev/null | python -mjson.tool; }
-function consumer.delete() { group=$1; instance=$2; curl -X DELETE -H "Content-Type: application/json" ${confluentRestUrl}/consumers/${group}/instances/${instance}; }
+function consumer.createbinary() { group=\$1; instance=\$2; echo '{ "id": "'$instance'", "format": "binary", "auto.offset.reset": "smallest", "auto.commit.enable": "false" }' > /tmp/consumer.create.$$.json; curl -X POST -H "Content-Type: application/vnd.kafka.binary.v1+json" --data-binary "@/tmp/consumer.create.$$.json" ${confluentRestUrl}/consumers/${group} 2>/dev/null | python -mjson.tool; }
+function consumer.createavro() { group=\$1; instance=\$2; echo '{ "id": "'$instance'", "format": "binary", "auto.offset.reset": "smallest", "auto.commit.enable": "false" }' > /tmp/consumer.create.$$.json; curl -X POST -H "Content-Type: application/vnd.kafka.avro.v1+json" --data-binary "@/tmp/consumer.create.$$.json" ${confluentRestUrl}/consumers/${group} 2>/dev/null | python -mjson.tool; }
+function consumer.readbinary() { group=\$1; instance=\$2; topic_name=$3; curl -X GET -H "Content-Type: application/vnd.kafka.binary.v2+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
+#function consumer.readavro() { group=\$1; instance=\$2; topic_name=$3; curl -X GET -H "Content-Type: application/vnd.kafka.avro.v1+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
+function consumer.readavro() { group=\$1; instance=\$2; topic_name=$3; curl -X GET -H "Accept: application/vnd.kafka.avro.v1+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
+function consumer.readavro() { group=\$1; instance=\$2; topic_name=$3; curl -X GET -H "Accept: application/vnd.kafka.v2+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
+function consumer.readavro() { group=\$1; instance=\$2; topic_name=$3; curl -X GET -H "Accept: application/vnd.kafka.binary.v1+json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/topics/${topic_name} 2>/dev/null | python -mjson.tool; }
+function consumer.commit() { group=\$1; instance=\$2; curl -X POST -H "Content-Type: application/json" ${confluentRestUrl}/consumers/${group}/instances/${instance}/offsets 2>/dev/null | python -mjson.tool; }
+function consumer.delete() { group=\$1; instance=\$2; curl -X DELETE -H "Content-Type: application/json" ${confluentRestUrl}/consumers/${group}/instances/${instance}; }
 # partition.produce-avro	POST /topics/{topic}/partitions/{partition} with Content-Type: application/vnd.kafka.avro.v1+json header
 # partition.produce-binary	POST /topics/{topic}/partitions/{partition} with Content-Type: application/vnd.kafka.binary.v1+json header
 # topic.produce-avro	POST /topics/{topic} with Content-Type: application/vnd.kafka.avro.v1+json header
@@ -587,9 +587,9 @@ function consumer.delete() { group=$1; instance=$2; curl -X DELETE -H "Content-T
 
 export schemaRegistryRestUrl=http://${HOSTNAME}:${schemaport1}
 function sr.subject.list() { curl -X GET -H "Accept: application/vnd.schemaregistry.v1+json" ${schemaRegistryRestUrl}/subjects 2>/dev/null | python -mjson.tool; }
-function sr.subject.versions() { subject=$1; curl -X GET -H "Accept: application/vnd.schemaregistry.v1+json" ${schemaRegistryRestUrl}/subjects/${subject}/versions 2>/dev/null | python -mjson.tool; }
-function sr.subject.getversion() { subject=$1; version=$2; curl -X GET -H "Accept: application/vnd.schemaregistry.v1+json" ${schemaRegistryRestUrl}/subjects/${subject}/versions/${version} 2>/dev/null | python -mjson.tool; }
-function sr.schema.get() { id=$1; curl -X GET -H "Accept: application/vnd.schemaregistry.v1+json" ${schemaRegistryRestUrl}/schemas/ids/${id} 2>/dev/null | python -mjson.tool; }
+function sr.subject.versions() { subject=\$1; curl -X GET -H "Accept: application/vnd.schemaregistry.v1+json" ${schemaRegistryRestUrl}/subjects/${subject}/versions 2>/dev/null | python -mjson.tool; }
+function sr.subject.getversion() { subject=\$1; version=\$2; curl -X GET -H "Accept: application/vnd.schemaregistry.v1+json" ${schemaRegistryRestUrl}/subjects/${subject}/versions/${version} 2>/dev/null | python -mjson.tool; }
+function sr.schema.get() { id=\$1; curl -X GET -H "Accept: application/vnd.schemaregistry.v1+json" ${schemaRegistryRestUrl}/schemas/ids/${id} 2>/dev/null | python -mjson.tool; }
 EOFenv
 
 ###########################################################
